dc805d01e1193a186bfb4eb104cb6ee68c58afc5
Adam Sears <adam_sears031@hotmail.com>
2023-03-31 04:41:55 -0400

    High Scores update
    Added the ability to save your high score by entering your initials
    
    also added functionality to restart the game or quit with a key

---
+++ SnakeV2/Classes/HighScores.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SnakeGame.Classes
+{
+    public class HighScores
+    {
+        private const string FileName = "highscores.txt";
+
+        public static void SaveScore(string initials, int score)
+        {
+            using (StreamWriter writer = File.AppendText(FileName))
+            {
+                writer.WriteLine($"{initials},{score}");
+            }
+        }
+
+        public static List<(string Initials, int Score)> LoadScores()
+        {
+            List<(string Initials, int Score)> highScores = new List<(string Initials, int Score)>();
+
+            if (File.Exists(FileName))
+            {
+                using (StreamReader reader = new StreamReader(FileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+
+                        if (parts.Length == 2)
+                        {
+                            string initials = parts[0];
+                            if (int.TryParse(parts[1], out int score))
+                            {
+                                highScores.Add((initials, score));
+                            }
+                        }
+                    }
+                }
+            }
+
+            return highScores;
+        }
+    }
+}
--- SnakeV2/Program.cs
+++ SnakeV2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using SnakeGame.Classes;
 
 namespace SnakeGame
@@ -7,33 +8,76 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
-            Console.SetWindowSize(50, 25);
-            Console.SetBufferSize(50, 25);
-
-            int width = Console.WindowWidth - 1;
-            int height = Console.WindowHeight - 1;
-
-            Game game = new Game(width, height);
-            Snake snake = game.Snake;
-            Food food = game.Food;
-
-            int directionX = 1;
-            int directionY = 0;
-
-            while (!game.GameOver)
+            while (true)
             {
-                // Update game logic
-                GameLogic.UpdateGame(game, snake, food, directionX, directionY);
+                Console.CursorVisible = false;
+                Console.SetWindowSize(50, 25);
+                Console.SetBufferSize(50, 25);
 
-                // Render the game
-                Renderer.Draw(game, snake, food);
+                int width = Console.WindowWidth - 1;
+                int height = Console.WindowHeight - 1;
 
-                // Update direction based on input
-                InputHandler.UpdateDirection(ref directionX, ref directionY);
+                Game game = new Game(width, height);
+                Snake snake = game.Snake;
+                Food food = game.Food;
 
-                // Sleep for a short time to control game speed
-                System.Threading.Thread.Sleep(game.SleepTime);
+                int directionX = 1;
+                int directionY = 0;
+
+                while (!game.GameOver)
+                {
+                    // Update game logic
+                    GameLogic.UpdateGame(game, snake, food, directionX, directionY);
+
+                    // Render the game
+                    Renderer.Draw(game, snake, food);
+
+                    // Update direction based on input
+                    InputHandler.UpdateDirection(ref directionX, ref directionY);
+
+                    // Sleep for a short time to control game speed
+                    System.Threading.Thread.Sleep(game.SleepTime);
+                }
+
+                // Show game over message
+                Console.Clear();
+                Console.WriteLine("Game Over!");
+                Console.WriteLine($"Score: {game.Score}");
+
+                // Get user initials and save the score
+                Console.Write("Enter your initials (3 characters): ");
+                Console.CursorVisible = true;
+                string input = Console.ReadLine();
+                string initials = input.ToUpper().Substring(0, Math.Min(3, input.Length));
+                Console.CursorVisible = false;
+
+                HighScores.SaveScore(initials, game.Score);
+
+                // Load and display high scores
+                Console.WriteLine("\nHigh Scores:");
+                var highScores = HighScores.LoadScores();
+                var orderedScores = highScores.OrderByDescending(x => x.Score).Take(10);
+
+                int rank = 1;
+                foreach (var score in orderedScores)
+                {
+                    string userIndicator = score.Initials == initials ? " <--- " + initials : "";
+                    Console.WriteLine($"{rank}. {score.Initials} - {score.Score}{userIndicator}");
+                    rank++;
+                }
+
+                // Prompt the user to restart or quit
+                Console.WriteLine("\nPress 'R' to restart or 'Q' to quit.");
+                ConsoleKey key;
+                do
+                {
+                    key = Console.ReadKey(true).Key;
+                } while (key != ConsoleKey.R && key != ConsoleKey.Q);
+
+                if (key == ConsoleKey.Q)
+                {
+                    break;
+                }
             }
         }
     }
